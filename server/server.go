087961ceb5422d5795873876bb643cd1dc763e42
Bruno Albuquerque <bga@gmail.com>
2022-01-21 11:39:19 -0800

    Add generic server implementation.
    - Supports stream and packet connections with the same API.
    - Callers only need to provide a ConnectionHandler function and handle
      what looks like a stream connection (even for UDP, for example).

---
+++ server/server.go
@@ -0,0 +1,205 @@
+package server
+
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// Server is a server that handles both packet and stream protocols with the
+// same API. There is some complexity in achieving that so I would not use
+// it for performance-critical servers (that is being said without any
+// performance testing being done whatsoever). Clients of the API just need
+// to provide a connection handler and just read/write data from/to the
+// associated connection (in exactly the same way for underlying stream or
+// packet connections).
+type Server struct {
+	network           string
+	address           string
+	connectionHandler ConnectionHandler
+
+	// For testing purposes only.
+	listen       func(string, string) (net.Listener, error)
+	listenPacket func(string, string) (net.PacketConn, error)
+
+	packetAddrConnMap map[string]net.Conn
+
+	wg sync.WaitGroup
+
+	m          sync.Mutex
+	listener   net.Listener   // nil if packetConn is not
+	packetConn net.PacketConn // nil if listener is not
+	started    bool
+}
+
+// ConnectionHandler is the signature for functions that that will handle
+// server connections. The handler is called on its own goroutine.
+type ConnectionHandler func(net.Conn)
+
+// New creates a new Server instance that will try to listen at the given
+// network and address and that will call the given connectionHandler to handle
+// incoming connections (and "fake" connections for packet connections).
+// Note that New only validates that connectionHandler is not nil. All other
+// errors will be reported when Start is called.
+func New(network, address string,
+	connectionHandler ConnectionHandler) (*Server, error) {
+	if connectionHandler == nil {
+		return nil, fmt.Errorf("connectionHandler cannot be nil")
+	}
+
+	return &Server{
+		network:           network,
+		address:           address,
+		connectionHandler: connectionHandler,
+		listen:            net.Listen,
+		listenPacket:      net.ListenPacket,
+		packetAddrConnMap: make(map[string]net.Conn),
+	}, nil
+}
+
+// Start tries to start listening for incoming connections. It returns a nil
+// error on success and a non-nil error on failure.
+func (s *Server) Start() error {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if s.started {
+		return fmt.Errorf("server already started")
+	}
+
+	switch s.network {
+	case "tcp", "tcp4", "tcp6", "unix", "unixpacket":
+		listener, err := s.listen(s.network, s.address)
+		if err != nil {
+			return err
+		}
+
+		s.listener = listener
+
+		s.wg.Add(1)
+		go s.listenLoop()
+	default:
+		packetConn, err := s.listenPacket(s.network, s.address)
+		if err != nil {
+			return err
+		}
+
+		s.packetConn = packetConn
+
+		s.wg.Add(1)
+		go s.packetListenLoop()
+	}
+
+	s.started = true
+
+	return nil
+}
+
+// Stop tries to stop listening for connections. It returns a nil error on
+// success and a non-nil error on failure.
+func (s *Server) Stop() error {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if !s.started {
+		return fmt.Errorf("server not started")
+	}
+
+	if s.listener != nil {
+		s.listener.Close()
+	} else if s.packetConn != nil {
+		s.packetConn.Close()
+	}
+
+	s.wg.Wait()
+
+	s.started = false
+	s.listener = nil
+	s.packetConn = nil
+
+	return nil
+}
+
+func (s *Server) listenLoop() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Temporary() {
+				continue
+			}
+			break
+		}
+
+		go s.connectionHandler(conn)
+	}
+
+	s.wg.Done()
+}
+
+func (s *Server) packetListenLoop() {
+	for {
+		buffer := make([]byte, 4096)
+		n, addr, err := s.packetConn.ReadFrom(buffer)
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Temporary() {
+				continue
+			}
+			break
+		}
+
+		var localConn, remoteConn net.Conn
+		var ok bool
+		if localConn, ok = s.packetAddrConnMap[addr.String()]; !ok {
+			localConn, remoteConn = net.Pipe()
+			s.packetAddrConnMap[addr.String()] = localConn
+
+			// Read from localConn and relay data to packetConn.
+			s.wg.Add(1)
+			go s.relayLoop(localConn, addr)
+
+			go s.connectionHandler(remoteConn)
+		}
+
+		_, err = localConn.Write(buffer[:n])
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Temporary() {
+				continue
+			}
+
+			break
+		}
+	}
+
+	for _, conn := range s.packetAddrConnMap {
+		conn.Close()
+	}
+
+	s.packetAddrConnMap = nil
+
+	s.wg.Done()
+}
+
+func (s *Server) relayLoop(localConn net.Conn, addr net.Addr) {
+	buffer := make([]byte, 4096)
+	for {
+		n, err := localConn.Read(buffer)
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Temporary() {
+				continue
+			}
+			break
+		}
+
+		_, err = s.packetConn.WriteTo(buffer[:n], addr)
+		if err != nil {
+			if opErr, ok := err.(*net.OpError); ok && opErr.Temporary() {
+				continue
+			}
+			break
+		}
+	}
+
+	localConn.Close()
+
+	s.wg.Done()
+}
---
+++ server/server_test.go
@@ -0,0 +1,342 @@
+package server
+
+import (
+	"fmt"
+	"net"
+	"testing"
+
+	testing2 "github.com/brunoga/net/testing"
+)
+
+func TestNew(t *testing.T) {
+	_, err := New("", "", nil)
+	if err == nil {
+		t.Error("expected non-nil error, got nil")
+	}
+
+	c, err := New("", "", func(net.Conn) {})
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+	if c == nil {
+		t.Error("expected non-nil client, got nil")
+	}
+}
+
+func TestStart_TCP(t *testing.T) {
+	s, err := New("tcp", "127.0.0.1:8080", func(net.Conn) {})
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	s.listen = func(string, string) (net.Listener, error) {
+		return nil, fmt.Errorf("listen error")
+	}
+
+	err = s.Start()
+	if err == nil {
+		t.Error("expected non-nil error, got nil")
+	}
+
+	ch := make(chan struct{})
+	s.listen = func(string, string) (net.Listener, error) {
+		return &testing2.MockListener{
+			AcceptFunc: func() (net.Conn, error) {
+				_ = <-ch
+
+				return nil, fmt.Errorf("accept error")
+			},
+		}, nil
+	}
+
+	err = s.Start()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	close(ch)
+
+	s.wg.Wait()
+}
+
+func TestStart_UDP(t *testing.T) {
+	s, err := New("udp", "127.0.0.1:8080", func(net.Conn) {})
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	s.listenPacket = func(string, string) (net.PacketConn, error) {
+		return nil, fmt.Errorf("listenPacket error")
+	}
+
+	err = s.Start()
+	if err == nil {
+		t.Error("expected non-nil error, got nil")
+	}
+
+	ch := make(chan struct{})
+	s.listenPacket = func(string, string) (net.PacketConn, error) {
+		return &testing2.MockPacketConn{
+			ReadFromFunc: func(b []byte) (int, net.Addr, error) {
+				<-ch
+				return 0, nil, fmt.Errorf("readFrom error")
+			},
+			WriteToFunc: func(b []byte, addr net.Addr) (int, error) {
+				<-ch
+				return 0, fmt.Errorf("writeTo error")
+			},
+		}, nil
+	}
+
+	err = s.Start()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	err = s.Start()
+	if err == nil {
+		t.Errorf("expected non-nil error, got nil")
+	}
+
+	// Unblock ReadFrom/WriteTo in the MockPacketConn.
+	close(ch)
+
+	s.wg.Wait()
+}
+
+func TestStop_TCP(t *testing.T) {
+	s, err := New("tcp", "127.0.0.1:8080", func(net.Conn) {})
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	ch := make(chan struct{})
+	s.listen = func(string, string) (net.Listener, error) {
+		return &testing2.MockListener{
+			AcceptFunc: func() (net.Conn, error) {
+				_ = <-ch
+
+				return nil, fmt.Errorf("accept error")
+			},
+			CloseFunc: func() error {
+				close(ch)
+
+				return nil
+			},
+		}, nil
+	}
+
+	err = s.Stop()
+	if err == nil {
+		t.Error("expected non-nil error, got nil")
+	}
+
+	err = s.Start()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	err = s.Stop()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+}
+
+func TestStop_UDP(t *testing.T) {
+	s, err := New("udp", "127.0.0.1:8080", func(net.Conn) {})
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	ch := make(chan struct{})
+	s.listenPacket = func(string, string) (net.PacketConn, error) {
+		return &testing2.MockPacketConn{
+			ReadFromFunc: func(b []byte) (int, net.Addr, error) {
+				<-ch
+				return 0, nil, fmt.Errorf("readFrom error")
+			},
+			WriteToFunc: func(b []byte, addr net.Addr) (int, error) {
+				<-ch
+				return 0, fmt.Errorf("writeTo error")
+			},
+			CloseFunc: func() error {
+				close(ch)
+				return nil
+			},
+		}, nil
+	}
+
+	err = s.Stop()
+	if err == nil {
+		t.Error("expected non-nil error, got nil")
+	}
+
+	err = s.Start()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	err = s.Stop()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+}
+
+func TestConnection_TCP(t *testing.T) {
+	handlerCh := make(chan []byte)
+	connectionHandler := func(conn net.Conn) {
+		buffer := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buffer)
+			if err != nil {
+				break
+			}
+
+			handlerCh <- buffer[:n]
+		}
+
+		conn.Close()
+	}
+
+	s, err := New("tcp", "", connectionHandler)
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	connCh := make(chan net.Conn)
+	s.listen = func(string, string) (net.Listener, error) {
+		return &testing2.MockListener{
+			AcceptFunc: func() (net.Conn, error) {
+				conn := <-connCh
+				if conn == nil {
+					return nil, fmt.Errorf("accept error")
+				}
+
+				return conn, nil
+			},
+			CloseFunc: func() error {
+				close(connCh)
+				return nil
+			},
+		}, nil
+	}
+
+	err = s.Start()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	localConn, remoteConn := net.Pipe()
+
+	connCh <- remoteConn
+
+	localConn.Write([]byte("hello"))
+
+	data := <-handlerCh
+	if string(data) != "hello" {
+		t.Errorf("expected 'hello', got %v", string(data))
+	}
+
+	defer s.Stop()
+}
+
+func TestConnection_UDP(t *testing.T) {
+	handlerCh := make(chan []byte)
+	connectionHandler := func(conn net.Conn) {
+		buffer := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buffer)
+			if err != nil {
+				break
+			}
+
+			handlerCh <- buffer[:n]
+
+			_, err = conn.Write([]byte("hello yourself"))
+			if err != nil {
+				break
+			}
+		}
+
+		conn.Close()
+	}
+
+	s, err := New("udp", "", connectionHandler)
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	type ConnData struct {
+		Addr net.Addr
+		Data []byte
+	}
+	readFromDataCh := make(chan *ConnData)
+	writeToDataCh := make(chan *ConnData)
+
+	packetConn := &testing2.MockPacketConn{
+		ReadFromFunc: func(b []byte) (int, net.Addr, error) {
+			connData := <-readFromDataCh
+			if connData == nil {
+				return 0, nil, fmt.Errorf("readfrom error")
+			}
+
+			copy(b, connData.Data)
+
+			return len(connData.Data), connData.Addr, nil
+		},
+		WriteToFunc: func(b []byte, addr net.Addr) (int, error) {
+			defer func() {
+				writeToDataCh <- &ConnData{
+					Addr: addr,
+					Data: b,
+				}
+			}()
+
+			return len(b), nil
+		},
+		CloseFunc: func() error {
+			close(readFromDataCh)
+			return nil
+		},
+	}
+
+	s.listenPacket = func(string, string) (net.PacketConn, error) {
+		return packetConn, nil
+	}
+
+	err = s.Start()
+	if err != nil {
+		t.Errorf("expected nil error, got %v", err)
+	}
+
+	readFromDataCh <- &ConnData{
+		Addr: &testing2.MockAddr{
+			NetworkFunc: func() string {
+				return "udp"
+			},
+			StringFunc: func() string {
+				return "x.x.x.x:yyyy"
+			},
+		},
+		Data: []byte("hello"),
+	}
+
+	data := <-handlerCh
+
+	if string(string(data)) != "hello" {
+		t.Errorf("expected 'hello', got %v", string(data))
+	}
+
+	writeToData := <-writeToDataCh
+	if writeToData.Addr.Network() != "udp" {
+		t.Errorf("expected 'udp', got %v", writeToData.Addr.Network())
+	}
+	if writeToData.Addr.String() != "x.x.x.x:yyyy" {
+		t.Errorf("expected 'x.x.x.x:yyyy', got %v", writeToData.Addr.String())
+	}
+	if string(writeToData.Data) != "hello yourself" {
+		t.Errorf("expected 'hello yourself', got %v", string(writeToData.Data))
+	}
+
+	defer s.Stop()
+}
